d7b56f380ae666540c857a064d4b1d0a160dc2fe
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2022-03-06 20:51:40 -0500

    Add tests from dast extension.

--- packages/mdast-heading-id/__tests__/basic.js
+++ packages/mdast-heading-id/__tests__/basic.js
@@ -1,5 +1,82 @@
+import {fromMarkdown} from 'mdast-util-from-markdown';
+import {micromarkHeadingId} from 'micromark-heading-id';
+import {mdastHeadingId} from '../index.js';
+
 describe('mdast plugin', () => {
-  it('noop', () => {
-    expect(true).toBeTruthy();
+  it('emits id node', () => {
+    expect(
+      fromMarkdown(`# simple id {#my-id-is-this-one}`, {
+        extensions: [micromarkHeadingId()],
+        mdastExtensions: [mdastHeadingId()],
+      })
+    ).toMatchInlineSnapshot(`
+      Object {
+        "children": Array [
+          Object {
+            "children": Array [
+              Object {
+                "position": Object {
+                  "end": Object {
+                    "column": 13,
+                    "line": 1,
+                    "offset": 12,
+                  },
+                  "start": Object {
+                    "column": 3,
+                    "line": 1,
+                    "offset": 2,
+                  },
+                },
+                "type": "text",
+                "value": "simple id ",
+              },
+              Object {
+                "position": Object {
+                  "end": Object {
+                    "column": 32,
+                    "line": 1,
+                    "offset": 31,
+                  },
+                  "start": Object {
+                    "column": 15,
+                    "line": 1,
+                    "offset": 14,
+                  },
+                },
+                "type": "idString",
+                "value": "my-id-is-this-one",
+              },
+            ],
+            "depth": 1,
+            "position": Object {
+              "end": Object {
+                "column": 33,
+                "line": 1,
+                "offset": 32,
+              },
+              "start": Object {
+                "column": 1,
+                "line": 1,
+                "offset": 0,
+              },
+            },
+            "type": "heading",
+          },
+        ],
+        "position": Object {
+          "end": Object {
+            "column": 33,
+            "line": 1,
+            "offset": 32,
+          },
+          "start": Object {
+            "column": 1,
+            "line": 1,
+            "offset": 0,
+          },
+        },
+        "type": "root",
+      }
+    `);
   });
 });
